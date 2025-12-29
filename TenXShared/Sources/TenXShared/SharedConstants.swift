aa090c9dfdec002da8f7663693f496dbba7fc01b
Matthew Lam <minghinmatthew.lam@gmail.com>
2025-12-29 00:10:37 -0800

    build: update bundle ids and app group

--- TenXShared/Sources/TenXShared/SharedConstants.swift
+++ TenXShared/Sources/TenXShared/SharedConstants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum SharedConstants {
-    public static let appGroupID = "group.com.yourname.tenx"
+    public static let appGroupID = "group.com.matthewlam.tenx"
     public static let widgetSnapshotFilename = "widget_snapshot.json"
 }
