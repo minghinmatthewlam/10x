c0fb964d8ce2d838dc56da4529d73307e587dbcd
Matthew Lam <minghinmatthew.lam@gmail.com>
2026-01-05 13:50:56 -0800

    feat: align non-success day colors

--- App/Features/YearProgress/YearProgressPalette.swift
+++ App/Features/YearProgress/YearProgressPalette.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 enum YearProgressPalette {
     static let success = Color(red: 0.30, green: 0.62, blue: 1.00)
-    static let incomplete = Color(red: 0.18, green: 0.38, blue: 0.78)
+    static let incomplete = Color(red: 0.06, green: 0.13, blue: 0.25)
     static let emptyToday = Color(red: 0.16, green: 0.27, blue: 0.48)
     static let emptyPast = Color(red: 0.06, green: 0.13, blue: 0.25)
     static let future = Color(red: 0.17, green: 0.17, blue: 0.18)
