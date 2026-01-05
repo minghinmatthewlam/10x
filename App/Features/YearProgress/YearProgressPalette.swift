9c14a03e1fe457e8038b28ac10b1f44da53ba551
Matthew Lam <minghinmatthew.lam@gmail.com>
2026-01-05 13:43:12 -0800

    feat: increase year contrast

--- App/Features/YearProgress/YearProgressPalette.swift
+++ App/Features/YearProgress/YearProgressPalette.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 enum YearProgressPalette {
-    static let success = Color(red: 0.22, green: 0.53, blue: 0.98)
+    static let success = Color(red: 0.30, green: 0.62, blue: 1.00)
     static let incomplete = Color(red: 0.18, green: 0.38, blue: 0.78)
     static let emptyToday = Color(red: 0.16, green: 0.27, blue: 0.48)
-    static let emptyPast = Color(red: 0.10, green: 0.18, blue: 0.32)
+    static let emptyPast = Color(red: 0.06, green: 0.13, blue: 0.25)
     static let future = Color(red: 0.17, green: 0.17, blue: 0.18)
 }
 
